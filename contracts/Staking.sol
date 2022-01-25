1e379d9495198f04e5f005ec016c6494b20ce7b6
Abhishek kochar <abhishekkochar2@Gmail.com>
2022-01-26 12:20:22 +1300

    Added function to deposit and withdraw ether

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -3,26 +3,40 @@ pragma solidity ^0.8.0;
 
 contract Staking{
     
+    address public owner;
+    address constant ETHER = address(0);
     //storing stakers addresses
     address[] public stakers;
-    mapping(address => uint256) public stakingBalance;
+    mapping(address => mapping(address=> uint256)) public stakingBalance;
+    //mapping(address => uint256) public stakingBalance;
     mapping(address => uint256) public rewardBalance;
-    mapping(address => bool) public hasStaked;
     mapping(address => bool) public  isStaking;
     
     //events
-    event Stake(address indexed from, uint256 amount);
-    event UnStake(address indexed from, uint256 amount);
+    event Stake(address indexed token, address indexed user, uint256 amount, uint256 balance);
+    event Withdraw(address indexed token, address indexed user, uint256 amount, uint256 balance);
 
     constructor( address _owner ){
-        _owner = msg.sender;
+        owner = _owner;
     }
 
-    function stakeToken() public{
+    function stakeETHER( address _token, uint256 _amount) payable public{
+        require(_token == ETHER, "Invalid Token");
+        stakingBalance[_token][msg.sender] = stakingBalance[_token][msg.sender]+(_amount);
+        isStaking[msg.sender] == true; 
+        emit Stake(_token, msg.sender, _amount, stakingBalance[_token][msg.sender] );
+    }
+
+    function withdrawETHER(uint256 _amount) public {
+        require(isStaking[msg.sender] == true, "No Eth to withdraw");
+        require(stakingBalance[ETHER][msg.sender]>=_amount);
+        stakingBalance[ETHER][msg.sender] =stakingBalance[ETHER][msg.sender]-(_amount);
+        payable(msg.sender).transfer(_amount);
+        emit Withdraw(ETHER, msg.sender, _amount, stakingBalance[ETHER][msg.sender]); 
 
     }
 
-    function unStake() public {
-
+    function balanceOf(address _user) public view returns (uint256){
+        return stakingBalance[ETHER][_user];
     }
 }
