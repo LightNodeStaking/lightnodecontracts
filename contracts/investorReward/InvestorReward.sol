d0b948a6b276c59ccc53fd148fe5689e8711ef14
Sol-Devloper <blockhunt14@gmail.com>
2022-03-29 19:59:27 +1300

    Adding/upfdating fuctions in the contract

--- contracts/investorReward/InvestorReward.sol
+++ contracts/investorReward/InvestorReward.sol
@@ -1,68 +1,94 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-//import "../token/SLETH.sol";
-
 contract InvestoReward {
-    uint256 admin;
+    //uint256 admin;
     address[] public adminList;
     mapping(address => bool) public isAdmin;
+    uint256 numberOfAdmin;
 
+    uint256 investorBalance;
     uint256 numberOfInvestor;
     address[] public investors;
     mapping(address => bool) public isInvestor;
 
-    constructor(address _admin) {
-        admin = _admin;
-        addAdmin();
+    constructor(address admin) {
+        admin = msg.sender;
     }
 
     /*modifier adminOnly(address _admin) {
         require(
             isAdmin[msg.sender] == true,
             "Only Admin can call this function"
+
         );
     }*/
 
-    function addInvestor(address _newInvestor) public {
+    function addInvestor(address _AddInvestor) public returns (string memory) {
         require(
             isAdmin[msg.sender] == true,
             "Only Admin can call this function"
         );
-        if (isInvestor[_newInvestor] = false) {
-            investors.push(_newInvestor);
-            isInvestor[_newInvestor] = true;
+        string memory messageInvestor = "The address is already an investor";
+        if (isInvestor[_AddInvestor] = false) {
+            investors.push(_AddInvestor);
+            isInvestor[_AddInvestor] = true;
             numberOfInvestor++;
-        } else if (isInvestor[_newInvestor] = true) returns (bool) {
-            return "User is already an Investor";
+        } else if (isInvestor[_AddInvestor] = true) {
+            return messageInvestor;
         }
     }
 
-    function removeInvestor() public {
+    function removeInvestor(address _AddInvestor)
+        public
+        returns (string memory)
+    {
         require(
             isAdmin[msg.sender] == true,
             "Only Admin can call this function"
         );
-    }
-
-    function addAdmin(address _newAdmin) public {
-        require(
-            isAdmin[msg.sender] == true,
-            "Only Admin can call this function"
-        );
-        if(isAdmin[_newAdmin] = false) {
-            adminlist.push(_newAdmin);
-            isAdmin[_newAdmin] = true;
-        } else if (isAdmin[_user] = true){
-
+        string
+            memory errorMessage = "The address is not an investor, please check";
+        if (isInvestor[_AddInvestor] = true) {
+            isInvestor[_AddInvestor] = false;
+            numberOfInvestor--;
+        } else if (isInvestor[_AddInvestor] = false) {
+            return errorMessage;
         }
-
     }
 
-    function removeAdmin(address _existingAdmin) public {
+    function addAdmin(address _additionalAdmin) public returns (string memory) {
         require(
             isAdmin[msg.sender] == true,
             "Only Admin can call this function"
         );
+        string memory messageAdmin = "The address is already an admin";
+        if (isAdmin[_additionalAdmin] = false) {
+            adminList.push(_additionalAdmin);
+            isAdmin[_additionalAdmin] = true;
+            numberOfAdmin++;
+        } else if (isAdmin[_additionalAdmin] = true) {
+            return messageAdmin;
+        }
     }
+
+    function removeAdmin(address _additionalAdmin)
+        public
+        returns (string memory)
+    {
+        require(
+            isAdmin[msg.sender] == true,
+            "Only Admin can call this function"
+        );
+        string
+            memory messageAdmin = "This address does not exist in admin directory";
+        if (isAdmin[_additionalAdmin] = true) {
+            isAdmin[_additionalAdmin] = false;
+            numberOfAdmin--;
+        } else if (isAdmin[_additionalAdmin] = false) {
+            return messageAdmin;
+        }
+    }
+
+    function investor
 }
