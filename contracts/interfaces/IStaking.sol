f5d86303636ef4e983a567afbc1760625e11e5e8
Sol-Devloper <blockhunt14@gmail.com>
2022-04-02 17:51:21 +1300

    issue #6 - Hardhat testing

--- contracts/interfaces/IStaking.sol
+++ contracts/interfaces/IStaking.sol
@@ -1,12 +1,12 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+// // SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.0;
 
-interface IStaking{
+// interface IStaking{
     
-    function depositEth1() payable external;
-    function setOwner(address _newOwner) external;
-    function setDevAddress(address _newDevAddress) external;    
-    function balanceOf(address _user) external view returns (uint256);
-    function stillStaking() external view returns (bool);
+//     function depositEth1() payable external;
+//     function setOwner(address _newOwner) external;
+//     function setDevAddress(address _newDevAddress) external;    
+//     function balanceOf(address _user) external view returns (uint256);
+//     function stillStaking() external view returns (bool);
     
-}
+// }
