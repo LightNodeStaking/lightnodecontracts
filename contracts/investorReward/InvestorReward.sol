14bd52075c17f6656353cf20d67fccf1c8a7259b
Sol-Devloper <blockhunt14@gmail.com>
2022-04-02 14:19:22 +1300

    Issue #6 - Investor reward contract

--- contracts/investorReward/InvestorReward.sol
+++ contracts/investorReward/InvestorReward.sol
@@ -7,22 +7,18 @@ contract InvestoReward {
     mapping(address => bool) public isAdmin;
     uint256 numberOfAdmin;
 
-    uint256 investorBalance;
+    //uint256 investorBalance;
     uint256 numberOfInvestor;
     address[] public investors;
     mapping(address => bool) public isInvestor;
 
+    uint256 setPercentage;
+    uint256 changePercentage;
+    mapping(address => uint256) public investorPercentage;
+
     constructor(address admin) {
         admin = msg.sender;
     }
-
-    /*modifier adminOnly(address _admin) {
-        require(
-            isAdmin[msg.sender] == true,
-            "Only Admin can call this function"
-
-        );
-    }*/
 
     function addInvestor(address _AddInvestor) public returns (string memory) {
         require(
@@ -45,7 +41,7 @@ contract InvestoReward {
     {
         require(
             isAdmin[msg.sender] == true,
-            "Only Admin can call this function"
+            "Only Admin can remove the investor"
         );
         string
             memory errorMessage = "The address is not an investor, please check";
@@ -57,35 +53,58 @@ contract InvestoReward {
         }
     }
 
-    function addAdmin(address _additionalAdmin) public returns (string memory) {
+    function addPercentage(address _investor) public returns (string memory) {
+        require(
+            isAdmin[msg.sender] == true,
+            "Only Admin can add/edit the percentage"
+        );
+        require(
+            isInvestor[_investor] == true,
+            "the existing address is not an investor"
+        );
+        setPercentage = setPercentage / 1000;
+        investorPercentage[_investor] = setPercentage;
+    }
+
+    function editPercentage(address _investor) public returns (string memory) {
+        require(
+            isAdmin[msg.sender] == true,
+            "Only Admin can add/edit the percentage"
+        );
+        require(
+            isInvestor[_investor] == true,
+            "the existing address is not an investor"
+        );
+        changePercentage = changePercentage / 1000;
+        investorPercentage[_investor] = changePercentage;
+    }
+
+    function addAdmin(address _admin) public returns (string memory) {
         require(
             isAdmin[msg.sender] == true,
             "Only Admin can call this function"
         );
         string memory messageAdmin = "The address is already an admin";
-        if (isAdmin[_additionalAdmin] = false) {
-            adminList.push(_additionalAdmin);
-            isAdmin[_additionalAdmin] = true;
+        if (isAdmin[_admin] = false) {
+            adminList.push(_admin);
+            isAdmin[_admin] = true;
             numberOfAdmin++;
-        } else if (isAdmin[_additionalAdmin] = true) {
+        } else if (isAdmin[_admin] = true) {
             return messageAdmin;
         }
     }
 
-    function removeAdmin(address _additionalAdmin)
-        public
-        returns (string memory)
-    {
+    function removeAdmin(address _admin) public returns (string memory) {
         require(
             isAdmin[msg.sender] == true,
             "Only Admin can call this function"
         );
         string
             memory messageAdmin = "This address does not exist in admin directory";
-        if (isAdmin[_additionalAdmin] = true) {
-            isAdmin[_additionalAdmin] = false;
+        if (isAdmin[_admin] = true) {
+            isAdmin[_admin] = false;
             numberOfAdmin--;
-        } else if (isAdmin[_additionalAdmin] = false) {
+        } else if (isAdmin[_admin] = false) {
             return messageAdmin;
         }
     }
