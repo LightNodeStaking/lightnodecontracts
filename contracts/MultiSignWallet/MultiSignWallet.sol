da2f7f1f8a91027bdec419525556e24ff0500623
Sol-Devloper <blockhunt14@gmail.com>
2022-04-27 23:53:49 +1200

    update in the contract

--- contracts/MultiSignWallet/MultiSignWallet.sol
+++ contracts/MultiSignWallet/MultiSignWallet.sol
@@ -1,35 +1,91 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-// contract MultiSignWallet {
-//     address[] public owners;
+contract MultiSignWallet {
+    address[] public owners;
 
-//     mapping(address => bool) public isOwner;
-//     uint256 public numOfConfirmationRequired;
-//     mapping(address => mapping(address => bool)) public isConfirmed;
+    mapping(address => bool) public isOwner;
+    uint256 public numOfConfirmationRequired;
+    mapping(address => mapping(address => bool)) public isConfirmed;
 
-//     Transaction[] public transaction;
+    Transaction[] public transactions;
 
-//     struct Transaction {
-//         address to;
-//         uint256 value;
-//         bytes data;
-//         bool executed;
-//         uint256 numOfConfirmation;
-//     }
+    struct Transaction {
+        address to;
+        uint256 value;
+        bytes data;
+        bool executed;
+        uint256 numOfConfirmations;
+    }
 
-//     modifier onlyOwner() {
-//         require(isOwner[msg.sender], "NOT_OWNER");
-//         _;
-//     }
+    constructor() public {}
 
-//     constructor() public {}
+    modifier onlyOwner() {
+        require(isOwner[msg.sender], "NOT_OWNER");
+        _;
+    }
 
-//     function submitTransaction() public onlyOwner {}
+    modifier transExist(uint256 _txIndex) {
+        require(_txIndex < transactions.length, "TRANSACTION_NOT_EXIST");
+        _;
+    }
 
-//     function confirmTransaction() public onlyOwner {}
+    modifier notExceuted(uint256 _txIndex) {
+        require(!transactions[_txIndex].executed, "ALREADY_EXECUTED");
+        _;
+    }
 
-//     function executeTransaction() public onlyOwner {}
+    modifier notConfirmed(uint256 _txIndex) {
+        require(!isConfirmed[_txIndex][msg.sender], "ALREADY_CONFIRMED");
+        _;
+    }
 
-//     function revokeTransaction() public onlyOwner {}
-// }
+    event SubmitTransaction(
+        address indexed owner,
+        uint256 indexed txIndex,
+        address indexed to,
+        uint256 value,
+        bytes data
+    );
+    event ConfirmTransaction();
+    event ExecuteTransaction();
+    event RevokeTransaction();
+
+    function submitTransaction(
+        address _to,
+        uint256 _value,
+        bytes memory _data
+    ) public onlyOwner {
+        uint256 txIndex = transactions.length;
+
+        transactions.push(
+            Transaction({
+                to: _to,
+                value: _value,
+                data: _data,
+                executed: false,
+                numOfConfirmations: 0
+            })
+        );
+
+        emit SubmitTransaction(msg.sender, txIndex, _to, _value, _data);
+    }
+
+    function confirmTransaction(uint256 _txIndex)
+        public
+        onlyOwner
+        transExist(_txIndex)
+        notExceuted(_txIndex)
+        notConfirmed(_txIndex)
+    {
+        emit ConfirmTransaction();
+    }
+
+    function executeTransaction() public onlyOwner {
+        emit ExecuteTransaction();
+    }
+
+    function revokeTransaction() public onlyOwner {
+        emit RevokeTransaction();
+    }
+}
