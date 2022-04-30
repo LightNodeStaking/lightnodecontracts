f356eab4ecccc52abcac9cf7ea518a3ce8a5b64d
Abhishek kochar <abhishekkochar2@Gmail.com>
2022-04-30 18:48:14 +1200

    LightNode contract

--- contracts/NodeOperators/NodeOperatorsRegistry.sol
+++ contracts/NodeOperators/NodeOperatorsRegistry.sol
@@ -6,51 +6,90 @@ import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "../lib/UnStructuredData.sol";
-import "../lib/";
+import "../lib/Memoryutils.sol";
+import "solidity-bytes-utils/contracts/BytesLib.sol";
 
 contract NodeOperatorsRegistry is INodeOperatorsRegistry, AccessControl{
     using SafeMath for uint256;
     using UnStructuredData for bytes32;
 
- //Account control List
-bytes32 constant public MANAGE_SIGNING_KEYS = keccak256("MANAGE_SIGNING_KEYS");
-bytes32 constant public ADD_NODE_OPERATOR_ROLE = keccak256("ADD_NODE_OPERATOR_ROLE");
-bytes32 constant public SET_NODE_OPERATOR_ACTIVE_ROLE = keccak256("SET_NODE_OPERATOR_ACTIVE_ROLE");
-bytes32 constant public SET_NODE_OPERATOR_NAME_ROLE = keccak256("SET_NODE_OPERATOR_NAME_ROLE");
-bytes32 constant public SET_NODE_OPERATOR_ADDRESS_ROLE = keccak256("SET_NODE_OPERATOR_ADDRESS_ROLE");
-bytes32 constant public SET_NODE_OPERATOR_LIMIT_ROLE = keccak256("SET_NODE_OPERATOR_LIMIT_ROLE");
-bytes32 constant public REPORT_STOPPED_VALIDATORS_ROLE = keccak256("REPORT_STOPPED_VALIDATORS_ROLE");
+    //Account control List
+    bytes32 constant public MANAGE_SIGNING_KEYS = keccak256("MANAGE_SIGNING_KEYS");
+    bytes32 constant public ADD_NODE_OPERATOR_ROLE = keccak256("ADD_NODE_OPERATOR_ROLE");
+    bytes32 constant public SET_NODE_OPERATOR_ACTIVE_ROLE = keccak256("SET_NODE_OPERATOR_ACTIVE_ROLE");
+    bytes32 constant public SET_NODE_OPERATOR_NAME_ROLE = keccak256("SET_NODE_OPERATOR_NAME_ROLE");
+    bytes32 constant public SET_NODE_OPERATOR_ADDRESS_ROLE = keccak256("SET_NODE_OPERATOR_ADDRESS_ROLE");
+    bytes32 constant public SET_NODE_OPERATOR_LIMIT_ROLE = keccak256("SET_NODE_OPERATOR_LIMIT_ROLE");
+    bytes32 constant public REPORT_STOPPED_VALIDATORS_ROLE = keccak256("REPORT_STOPPED_VALIDATORS_ROLE");
 
-uint256 constant public PUBKEY_LENGTH = 48;
-uint256 constant public SIGNATURE_LENGTH = 96;
+    uint256 constant public PUBKEY_LENGTH = 48;
+    uint256 constant public SIGNATURE_LENGTH = 96;
 
-uint256 internal constant UINT64_MAX = uint256(uint64(-1));
-bytes32 internal constant SIGNING_KEYS_MAPPING_NAME = keccak256("lightNode.NodeOperatorsRegistry.signingKeysMappingName");
+    uint256 internal constant UINT64_MAX = uint256(uint64(-1));
+    bytes32 internal constant SIGNING_KEYS_MAPPING_NAME = keccak256("lightNode.NodeOperatorsRegistry.signingKeysMappingName");
 
 
-// @dev Node Operator parameters and internal state
-struct NodeOperator {
-    bool active;    // a flag indicating if the operator can participate in further staking and reward distribution
-    address rewardAddress;  // Ethereum 1 address which receives steth rewards for this operator
-    string name;    // human-readable name
-    uint64 stakingLimit;    // the maximum number of validators to stake for this operator
-    uint64 stoppedValidators;   // number of signing keys which stopped validation (e.g. were slashed)
+    // @dev Node Operator parameters and internal state
+    struct NodeOperator {
+        bool active;    // a flag indicating if the operator can participate in further staking and reward distribution
+        address rewardAddress;  // Ethereum 1 address which receives steth rewards for this operator
+        string name;    // human-readable name
+        uint64 stakingLimit;    // the maximum number of validators to stake for this operator
+        uint64 stoppedValidators;   // number of signing keys which stopped validation (e.g. were slashed)
 
-    uint64 totalSigningKeys;    // total amount of signing keys of this operator
-    uint64 usedSigningKeys;     // number of signing keys of this operator which were used in deposits to the Ethereum 2
-}
+        uint64 totalSigningKeys;    // total amount of signing keys of this operator
+        uint64 usedSigningKeys;     // number of signing keys of this operator which were used in deposits to the Ethereum 2
+    }
 
-// @dev Memory cache entry used in the assignNextKeys function
-struct DepositLookupCacheEntry {
-    // Makes no sense to pack types since reading memory is as fast as any op
-    uint256 id;
-    uint256 stakingLimit;
-    uint256 stoppedValidators;
-    uint256 totalSigningKeys;
-    uint256 usedSigningKeys;
-    uint256 initialUsedSigningKeys;
-}
-// @dev Mapping of all node operators. Mapping is used to be able to extend the struct.
-mapping(uint256 => NodeOperator) internal operators;
+    // @dev Memory cache entry used in the assignNextKeys function
+    struct DepositLookupCacheEntry {
+        // Makes no sense to pack types since reading memory is as fast as any op
+        uint256 id;
+        uint256 stakingLimit;
+        uint256 stoppedValidators;
+        uint256 totalSigningKeys;
+        uint256 usedSigningKeys;
+        uint256 initialUsedSigningKeys;
+    }
+    // @dev Mapping of all node operators. Mapping is used to be able to extend the struct.
+    mapping(uint256 => NodeOperator) internal operators;
+
+    // @dev Total number of operators
+    bytes32 internal constant TOTAL_OPERATORS_COUNT_POSITION = keccak256("lightNode.NodeOperatorsRegistry.totalOperatorsCount");
+
+    // @dev Cached number of active operators
+    bytes32 internal constant ACTIVE_OPERATORS_COUNT_POSITION = keccak256("lightNode.NodeOperatorsRegistry.activeOperatorsCount");
+
+    /// @dev link to the Lido contract
+    bytes32 internal constant LIGHT_NODE_POSITION = keccak256("lightNode.NodeOperatorsRegistry.lightNode");
+
+    /// @dev link to the index of operations with keys
+    bytes32 internal constant KEYS_OP_INDEX_POSITION = keccak256("lightNode.NodeOperatorsRegistry.keysOpIndex");
+
+    modifier onlyLightNode() {
+        require(msg.sender == LIGHT_NODE_POSITION.getStorageAddress(), "APP_AUTH_FAILED");
+        _;
+    }
+
+    modifier validAddress(address _a) {
+        require(_a != address(0), "INVALID_ADDRESS");
+        _;
+    }
+
+    modifier operatorExist( uint256 _id){
+        require(_id < getNodeOperatorsCount(), "NODE_OPERATOT_DOESN'T EXTIST");
+
+    }
+
+    function initialize(address _slEth) onlyInit{
+
+    }
+
+   /**
+      * @notice Returns total number of node operators
+      */
+    function getNodeOperatorsCount() public view returns (uint256) {
+        return TOTAL_OPERATORS_COUNT_POSITION.getStorageUint256();
+    }
 }
 
