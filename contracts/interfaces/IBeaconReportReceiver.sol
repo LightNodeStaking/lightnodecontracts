b12e545b5f708c0d7218eecd4fbc0d1f7171b7b4
Abhishek kochar <abhishekkochar2@Gmail.com>
2022-06-14 19:58:46 +1200

    Added beacon interface

---
+++ contracts/interfaces/IBeaconReportReceiver.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/**
+  * @title Interface defining a callback that the quorum will call on every quorum reached
+  */
+interface IBeaconReportReceiver {
+    /**
+      * @notice Callback to be called by the oracle contract upon the quorum is reached
+      * @param _postTotalPooledEther total pooled ether on Lido right after the quorum value was reported
+      * @param _preTotalPooledEther total pooled ether on Lido right before the quorum value was reported
+      * @param _timeElapsed time elapsed in seconds between the last and the previous quorum
+      */
+    function processLidoOracleReport(uint256 _postTotalPooledEther, uint256 _preTotalPooledEther, uint256 _timeElapsed) external;
+}
