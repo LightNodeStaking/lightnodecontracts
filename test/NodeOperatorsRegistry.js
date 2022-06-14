2889d2e327e463b5987e748e49b575deb1872e99
Sol-Devloper <blockhunt14@gmail.com>
2022-06-14 23:57:04 +1200

    Js file for testing

---
+++ test/NodeOperatorsRegistry.js
@@ -0,0 +1,8 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const { expectRevert } = require('@openzeppelin/test-helpers');
+const Table = require("cli-table3");
+
+describe("Node testing", function () {
+
+})
