c9125c3e9be0ad6704d0db9099d4f32397fc25dd
Sol-Devloper <blockhunt14@gmail.com>
2022-06-25 20:56:34 +1200

    Strtaing the Node Operator Registry contract testing

--- test/NodeOperatorsRegistry.js
+++ test/NodeOperatorsRegistry.js
@@ -5,4 +5,19 @@ const Table = require("cli-table3");
 
 describe("Node testing", function () {
 
+    beforeEach(async function () {
+        [SlEthOwner, stakingOwner, devFee, acc1, acc2] = await ethers.getSigners();
+
+        //deploying SLeth contract
+        const nodeRegistry = await hre.ethers.getContractFactory("NodeOperatorsRegistry");
+        registry = await nodeRegistry.deploy(SlEthOwner.address);
+        await registry.deployed();
+
+
+        // table.push(
+        //     ["SLETH token deployed at: " , slEth.address],
+        //     ["SlEth owner is: " , SlEthOwner.address],
+        // )
+    })
+
 })
