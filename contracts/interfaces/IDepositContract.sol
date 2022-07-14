819ef94308828500f5e194b6e83a8d5c40fed723
SmartCoding51 <keinakano415@gmail.com>
2022-07-15 01:40:47 +0900

    refactor interfaces for DepositContract & NodeOperatorsRegistry

--- contracts/interfaces/IDepositContract.sol
+++ contracts/interfaces/IDepositContract.sol
@@ -1,19 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+/// @title Deposit contract interface
 interface IDepositContract{
-    /*
-    @param pubKey validator siging key - bytes48
-    @param withdrawalCredentials that allows to withdraw funds - bytes32
-    @param sign signature of the request - bytes96
-    @param deposit_data_root The deposits Merkle tree node, used as a checksum
-    */
-
+    /**
+     * @notice Top-ups deposit of a validator on the ETH 2.0 side
+     * @param pubkey Validator signing key
+     * @param withdrawal_credentials Credentials that allows to withdraw funds
+     * @param signature Signature of the request
+     * @param deposit_data_root The deposits Merkle tree node, used as a checksum
+     */
     function deposit(
         bytes /* 48 */ memory pubkey,
         bytes /* 32 */ memory withdrawal_credentials,
         bytes /* 96 */ memory signature,
         bytes32 deposit_data_root
-    )
-        external payable;
+    ) external payable;
 }
