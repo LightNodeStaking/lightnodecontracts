4b18e9bbbb6d51127183e0730bb2bfdf4beae1f7
SmartCoding51 <keinakano415@gmail.com>
2022-07-16 00:22:30 +0900

    modify SlETH contract & interface

--- contracts/interfaces/ISLETH.sol
+++ contracts/interfaces/ISLETH.sol
@@ -1,37 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-interface ISLETH {
-    //function balanceOf(address owner) external view returns (uint256);
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-    /*function transfer(address recipient, uint256 amount)
-        external
-        returns (bool);*/
-
-    /*function allowance(address owner, address spender)
-        external
-        returns (uint256);*/
-
-    /*function approve(address spender, uint256 amount) external returns (bool);*/
-
-    /*function transferFrom(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) external returns (bool);*/
-
-    function increaseAllowance(address spender, uint256 addedValue)
-        external
-        returns (bool);
-
-    function decreaseAllowance(address spender, uint256 subtractedValue)
-        external
-        returns (bool);
-
-    function getTotalPooledEther() external view returns (uint256);
-
-    function getTotalShares() external view returns (uint256);
-
+interface ISlETH is IERC20 {
     function getPooledEthByShares(uint256 _sharesAmount)
         external
         view
--- contracts/token/SLETH.sol
+++ contracts/token/SLETH.sol
@@ -1,147 +1,247 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
-import "../lib/UnStructuredData.sol";
-import "../interfaces/ISLETH.sol";
+import "../lib/UnstructuredStorage.sol";
+import "../interfaces/ISlETH.sol";
 
-abstract contract SLETH is ISLETH, IERC20, Pausable {
-    using UnStructuredData for bytes32;
-    mapping(address => uint256) public _balances;
+abstract contract SlETH is ISlETH, Pausable {
+    using UnstructuredStorage for bytes32;
 
-    mapping(address => mapping(address => uint256)) private _allowances;
+    /**
+     * @dev SlETH balances are dynamic and are calculated based on the accounts' shares
+     * and the total amount of Ether controlled by the protocol. Account shares aren't
+     * normalized, so the contract also stores the sum of all shares to calculate
+     * each account's token balance which equals to:
+     *
+     *   shares[account] * _getTotalPooledEther() / _getTotalShares()
+     */
+    mapping(address => uint256) private shares;
 
-    bytes32 internal constant TOTAL_SHARES_POSITION =
-        keccak256("StETH.totalShares");
+    /**
+     * @dev Allowances are nominated in tokens, not token shares.
+     */
+    mapping(address => mapping(address => uint256)) private allowances;
 
-    string public _name = "Lightnode staked Ether";
-    string public _symbol = "slETH";
-    uint8 public _decimal = 18;
+    /**
+     * @dev Storage position used for holding the total amount of shares in existence.
+     */
+    bytes32 internal constant TOTAL_SHARES_POSITION = keccak256("lightnode.SlETH.totalShares");
 
-    //address public tokenAccount;
+    /**
+      * @notice An executed shares transfer from `sender` to `recipient`.
+      *
+      * @dev emitted in pair with an ERC20-defined `Transfer` event.
+      */
+    event TransferShares(
+        address indexed from,
+        address indexed to,
+        uint256 sharesValue
+    );
 
+    string private _name = "Lightnode staked Ether";
+    string private _symbol = "slETH";
+    /**
+     * @return the name of the token.
+     */
     function name() public view virtual returns (string memory) {
         return _name;
     }
 
+    /**
+     * @return the symbol of the token, usually a shorter version of the name.
+     */
     function symbol() public view virtual returns (string memory) {
         return _symbol;
     }
 
+    /**
+     * @return the number of decimals for getting user representation of a token amount.
+     */
     function decimals() public view virtual returns (uint8) {
-        return _decimal;
+        return 18;
     }
 
+    /**
+     * @return the amount of tokens in existence.
+     *
+     * @dev Always equals to `_getTotalPooledEther()` since token amount
+     * is pegged to the total amount of Ether controlled by the protocol.
+     */
     function totalSupply() public view virtual override returns (uint256) {
         return _getTotalPooledEther();
     }
 
-    function getTotalPooledEther() public view override returns (uint256) {
+    /**
+     * @return the entire amount of Ether controlled by the protocol.
+     *
+     * @dev The sum of all ETH balances in the protocol, equals to the total supply of slETH.
+     */
+    function getTotalPooledEther() public view returns (uint256) {
         return _getTotalPooledEther();
     }
 
-    function balanceOf(address account)
-        public
-        view
-        virtual
-        override
-        returns (uint256)
-    {
-        return getPooledEthByShares(_sharesOf(account));
+    /**
+     * @return the amount of tokens owned by the `_account`.
+     *
+     * @dev Balances are dynamic and equal the `_account`'s share in the amount of the
+     * total Ether controlled by the protocol. See `sharesOf`.
+     */
+    function balanceOf(address _account) public view virtual override returns (uint256) {
+        return getPooledEthByShares(_sharesOf(_account));
     }
 
-    function transfer(address recipient, uint256 amount)
+    /**
+     * @notice Moves `_amount` tokens from the caller's account to the `_recipient` account.
+     *
+     * @return a boolean value indicating whether the operation succeeded.
+     * Emits a `Transfer` event.
+     * Emits a `TransferShares` event.
+     *
+     * Requirements:
+     *
+     * - `_recipient` cannot be the zero address.
+     * - the caller must have a balance of at least `_amount`.
+     * - the contract must not be paused.
+     *
+     * @dev The `_amount` argument is the amount of tokens, not shares.
+     */
+    function transfer(address _recipient, uint256 _amount) public virtual override returns (bool) {
+        _transfer(msg.sender, _recipient, _amount);
+        return true;
+    }
+
+    /**
+     * @return the remaining number of tokens that `_spender` is allowed to spend
+     * on behalf of `_owner` through `transferFrom`. This is zero by default.
+     * @dev This value changes when `approve` or `transferFrom` is called.
+     */
+    function allowance(address _owner, address _spender) public view virtual override returns (uint256) {
+        return allowances[_owner][_spender];
+    }
+
+    /**
+     * @notice Sets `_amount` as the allowance of `_spender` over the caller's tokens.
+     *
+     * @return a boolean value indicating whether the operation succeeded.
+     * Emits an `Approval` event.
+     *
+     * Requirements:
+     *
+     * - `_spender` cannot be the zero address.
+     * - the contract must not be paused.
+     *
+     * @dev The `_amount` argument is the amount of tokens, not shares.
+     */
+    function approve(address _spender, uint256 _amount)
         public
         virtual
         override
         returns (bool)
     {
-        _transfer(msg.sender, recipient, amount);
+        _approve(msg.sender, _spender, _amount);
         return true;
     }
 
-    function allowance(address owner, address spender)
-        public
-        view
-        virtual
-        override
-        returns (uint256)
-    {
-        return _allowances[owner][spender];
-    }
-
-    function approve(address spender, uint256 amount)
-        public
-        virtual
-        override
-        returns (bool)
-    {
-        _approve(msg.sender, spender, amount);
-        return true;
-    }
-
+    /**
+     * @notice Moves `_amount` tokens from `_sender` to `_recipient` using the
+     * allowance mechanism. `_amount` is then deducted from the caller's
+     * allowance.
+     *
+     * @return a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a `Transfer` event.
+     * Emits a `TransferShares` event.
+     * Emits an `Approval` event indicating the updated allowance.
+     *
+     * Requirements:
+     *
+     * - `_sender` and `_recipient` cannot be the zero addresses.
+     * - `_sender` must have a balance of at least `_amount`.
+     * - the caller must have allowance for `_sender`'s tokens of at least `_amount`.
+     * - the contract must not be paused.
+     *
+     * @dev The `_amount` argument is the amount of tokens, not shares.
+     */
     function transferFrom(
-        address sender,
-        address recipient,
-        uint256 amount
+        address _sender,
+        address _recipient,
+        uint256 _amount
     ) public virtual override whenNotPaused returns (bool) {
-        uint256 currentAllowance = _allowances[sender][msg.sender];
-        if (currentAllowance != type(uint256).max) {
-            require(
-                currentAllowance >= amount,
-                "ERC20: transfer amount exceeds allowance"
-            );
-            unchecked {
-                _approve(sender, msg.sender, currentAllowance - amount);
-            }
-        }
-
-        _transfer(sender, recipient, amount);
-
-        return true;
-    }
-
-    function increaseAllowance(address spender, uint256 addedValue)
-        public
-        virtual
-        override
-        returns (bool)
-    {
-        _approve(
-            msg.sender,
-            spender,
-            _allowances[msg.sender][spender] + addedValue
-        );
-        return true;
-    }
-
-    function decreaseAllowance(address spender, uint256 subtractedValue)
-        public
-        virtual
-        override
-        returns (bool)
-    {
-        uint256 currentAllowance = _allowances[msg.sender][spender];
-        require(
-            currentAllowance >= subtractedValue,
-            "ERC20: DECREASED_ALLOWANCE_BELOW_ZERO"
-        );
+        uint256 currentAllowance = allowances[_sender][msg.sender];
+        require(currentAllowance >= _amount, "Insufficient allowance");
         unchecked {
-            _approve(msg.sender, spender, currentAllowance - subtractedValue);
+            _approve(_sender, msg.sender, currentAllowance - _amount);
         }
-
+        _transfer(_sender, _recipient, _amount);
         return true;
     }
 
-    function getTotalShares() public view virtual override returns (uint256) {
+    /**
+     * @notice Atomically increases the allowance granted to `_spender` by the caller by `_addedValue`.
+     *
+     * This is an alternative to `approve` that can be used as a mitigation for
+     * problems described in:
+     * https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol#L42
+     * Emits an `Approval` event indicating the updated allowance.
+     *
+     * Requirements:
+     *
+     * - `_spender` cannot be the the zero address.
+     * - the contract must not be paused.
+     */
+    function increaseAllowance(address _spender, uint256 _addedValue) public virtual returns (bool) {
+        _approve(msg.sender, _spender, allowances[msg.sender][_spender] + _addedValue);
+        return true;
+    }
+
+    /**
+     * @notice Atomically decreases the allowance granted to `_spender` by the caller by `_subtractedValue`.
+     *
+     * This is an alternative to `approve` that can be used as a mitigation for
+     * problems described in:
+     * https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol#L42
+     * Emits an `Approval` event indicating the updated allowance.
+     *
+     * Requirements:
+     *
+     * - `_spender` cannot be the zero address.
+     * - `_spender` must have allowance for the caller of at least `_subtractedValue`.
+     * - the contract must not be paused.
+     */
+    function decreaseAllowance(address _spender, uint256 _subtractedValue)
+        public
+        virtual
+        returns (bool)
+    {
+        uint256 currentAllowance = allowances[msg.sender][_spender];
+        require(currentAllowance >= _subtractedValue, "DECREASED_ALLOWANCE_BELOW_ZERO");
+        unchecked {
+            _approve(msg.sender, _spender, currentAllowance - _subtractedValue);
+        }
+        return true;
+    }
+
+    /**
+     * @return the total amount of shares in existence.
+     *
+     * @dev The sum of all accounts' shares can be an arbitrary number, therefore
+     * it is necessary to store it in order to calculate each account's relative share.
+     */
+    function getTotalShares() public view virtual returns (uint256) {
         return _getTotalShares();
     }
 
+    /**
+     * @return the amount of shares owned by `_account`.
+     */
     function sharesOf(address _account) public view returns (uint256) {
         return _sharesOf(_account);
     }
 
+    /**
+     * @return the amount of shares that corresponds to `_ethAmount` protocol-controlled Ether.
+     */
     function getSharesByPooledEth(uint256 _ethAmount)
         public
         view
@@ -152,10 +252,13 @@ abstract contract SLETH is ISLETH, IERC20, Pausable {
         if (totalPooledEther == 0) {
             return 0;
         } else {
-            return (_ethAmount * (_getTotalShares())) / (totalPooledEther);
+            return (_ethAmount * _getTotalShares()) / totalPooledEther;
         }
     }
 
+    /**
+     * @return the amount of Ether that corresponds to `_sharesAmount` token shares.
+     */
     function getPooledEthByShares(uint256 _sharesAmount)
         public
         view
@@ -166,37 +269,50 @@ abstract contract SLETH is ISLETH, IERC20, Pausable {
         if (totalShares == 0) {
             return 0;
         } else {
-            return (_sharesAmount * (_getTotalPooledEther())) / (totalShares);
+            return (_sharesAmount * _getTotalPooledEther()) / totalShares;
         }
     }
 
+    /**
+     * @notice Moves `_amount` tokens from `_sender` to `_recipient`.
+     * Emits a `Transfer` event.
+     * Emits a `TransferShares` event.
+     */
+    function _transfer(
+        address _sender,
+        address _recipient,
+        uint256 _amount
+    ) internal virtual whenNotPaused {
+        uint256 _sharesToTransfer = getSharesByPooledEth(_amount);
+        _transferShares(_sender, _recipient, _sharesToTransfer);
+        emit Transfer(_sender, _recipient, _amount);
+        emit TransferShares(_sender, _recipient, _sharesToTransfer);
+    }
+
+    /**
+     * @return the total amount of shares in existence.
+     */
     function _getTotalShares() internal view returns (uint256) {
         return TOTAL_SHARES_POSITION.getStorageUint256();
     }
 
+    /**
+     * @return the amount of shares owned by `_account`.
+     */
     function _sharesOf(address _account) internal view returns (uint256) {
-        return _balances[_account];
+        return shares[_account];
     }
 
     /**
-     * @return the total amount (in wei) of Ether controlled by the protocol.
-     * @dev This is used for calculating tokens from shares and vice versa.
-     * @dev This function is required to be implemented in a derived contract.
+     * @notice Moves `_sharesAmount` shares from `_sender` to `_recipient`.
+     *
+     * Requirements:
+     *
+     * - `_sender` cannot be the zero address.
+     * - `_recipient` cannot be the zero address.
+     * - `_sender` must hold at least `_sharesAmount` shares.
+     * - the contract must not be paused.
      */
-
-    function _getTotalPooledEther() internal view virtual returns (uint256);
-
-    function _transfer(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) internal virtual whenNotPaused {
-        uint256 _sharesToTransfer = getSharesByPooledEth(amount);
-        _transferShares(sender, recipient, _sharesToTransfer);
-
-        emit Transfer(sender, recipient, amount);
-    }
-
     function _transferShares(
         address _sender,
         address _recipient,
@@ -205,24 +321,18 @@ abstract contract SLETH is ISLETH, IERC20, Pausable {
         require(_sender != address(0), "TRANSFER_FROM_THE_ZERO_ADDRESS");
         require(_recipient != address(0), "TRANSFER_TO_THE_ZERO_ADDRESS");
 
-        _beforeTokenTransfer(_sender, _recipient, _sharesAmount);
+        uint256 currentSenderShares = shares[_sender];
+        require(currentSenderShares >= _sharesAmount, "transfer amount exceeds balance");
 
-        uint256 senderBalance = _balances[_sender];
-        require(
-            senderBalance >= _sharesAmount,
-            "ERC20: transfer amount exceeds balance"
-        );
-
-        _balances[_sender] = senderBalance - _sharesAmount;
-
-        _balances[_recipient] += _sharesAmount;
-
-        _afterTokenTransfer(_sender, _recipient, _sharesAmount);
+        unchecked {
+            shares[_sender] = currentSenderShares - _sharesAmount;
+            shares[_recipient] += _sharesAmount;
+        }
     }
 
     //mint function, mints new shares. it does not change the totalsupply
 
-    function _mint(address account, uint256 amount)
+    /* function _mint(address account, uint256 amount)
         public
         virtual
         whenNotPaused
@@ -267,37 +377,35 @@ abstract contract SLETH is ISLETH, IERC20, Pausable {
         emit Transfer(account, address(0), amount);
 
         _afterTokenTransfer(account, address(0), amount);
-    }
+    } */
 
+    /**
+     * @notice Sets `_amount` as the allowance of `_spender` over the `_owner` s tokens.
+     *
+     * Emits an `Approval` event.
+     *
+     * Requirements:
+     *
+     * - `_owner` cannot be the zero address.
+     * - `_spender` cannot be the zero address.
+     * - the contract must not be paused.
+     */
     function _approve(
-        address owner,
-        address spender,
-        uint256 amount
+        address _owner,
+        address _spender,
+        uint256 _amount
     ) internal virtual whenNotPaused {
-        require(owner != address(0), "ERC20: approve from the zero address");
-        require(spender != address(0), "ERC20: approve to the zero address");
+        require(_owner != address(0), "APPROVE_FROM_ZERO_ADDRESS");
+        require(_spender != address(0), "APPROVE_TO_ZERO_ADDRESS");
 
-        _allowances[owner][spender] = amount;
-        emit Approval(owner, spender, amount);
+        allowances[_owner][_spender] = _amount;
+        emit Approval(_owner, _spender, _amount);
     }
 
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal virtual {}
-
-    function _afterTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal virtual {}
-
-    function pause() external {
-        _pause();
-    }
-
-    function unpause() external {
-        _unpause();
-    }
+    /**
+     * @return the total amount (in wei) of Ether controlled by the protocol.
+     * @dev This is used for calculating tokens from shares and vice versa.
+     * @dev This function is required to be implemented in a derived contract.
+     */
+    function _getTotalPooledEther() internal view virtual returns (uint256);
 }
