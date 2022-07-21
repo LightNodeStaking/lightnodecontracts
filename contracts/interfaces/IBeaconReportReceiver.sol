2d832ab0703ec59a147f89835c820eb224febfbb
SmartCoding51 <keinakano415@gmail.com>
2022-07-22 01:07:47 +0900

    code refactor & change folder structure

--- contracts/interfaces/IBeaconReportReceiver.sol
+++ contracts/interfaces/IBeaconReportReceiver.sol
@@ -7,9 +7,13 @@ pragma solidity ^0.8.0;
 interface IBeaconReportReceiver {
     /**
       * @notice Callback to be called by the oracle contract upon the quorum is reached
-      * @param _postTotalPooledEther total pooled ether on Lido right after the quorum value was reported
-      * @param _preTotalPooledEther total pooled ether on Lido right before the quorum value was reported
+      * @param _postTotalPooledEther total pooled ether on LightNode right after the quorum value was reported
+      * @param _preTotalPooledEther total pooled ether on LightNode right before the quorum value was reported
       * @param _timeElapsed time elapsed in seconds between the last and the previous quorum
       */
-    function processLidoOracleReport(uint256 _postTotalPooledEther, uint256 _preTotalPooledEther, uint256 _timeElapsed) external;
+    function processOracleReport(
+      uint256 _postTotalPooledEther, 
+      uint256 _preTotalPooledEther, 
+      uint256 _timeElapsed
+    ) external;
 }
