86db0ae0219c84e1878950ccc58ad44da6e1bb54
SmartCoding51 <keinakano415@gmail.com>
2022-07-29 04:37:57 +0900

    deploy Oracle contract into the Goerli testnet

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -9,26 +9,15 @@ async function main() {
   // If this script is run directly using `node` you may want to call compile
   // manually to make sure everything is compiled
   // await hre.run('compile');
-  let SlEthOwner, stakingOwner, devFee ;
-  [SlEthOwner, stakingOwner, devFee] = await ethers.getSigners();
-//console.log(defoOwner.address)
+  let oracle;
+  let owner;
+  [owner] = await ethers.getSigners();
 
-  // deploying defo contract
-  const SlEth = await hre.ethers.getContractFactory("SLETH");
-  const slEth = await SlEth.deploy(SlEthOwner.address);
-  await slEth.deployed();
-  console.log("slEth token deployed at:", slEth.address);
-  console.log("slEth Owner is: " + SlEthOwner.address);
+  const Oracle = await hre.ethers.getContractFactory("Oracle");
+  oracle = await Oracle.deploy();
+  await oracle.deployed();
 
-  const StakingContract = await hre.ethers.getContractFactory("Staking");
-  const stakingContract = await StakingContract.deploy
-                  ( stakingOwner.address,
-                    devFee.address,
-                    slEth.address );
-  await stakingContract.deployed();
-  console.log("Staking contract deployed at:", stakingContract.address);
-  console.log("Staking Owner Owner is: " + stakingOwner.address);
-
+  console.log("Oracle deployed at:", oracle.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
