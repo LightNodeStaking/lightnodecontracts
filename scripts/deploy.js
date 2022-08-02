40158d3cb10f107ba0eb09b72b1448707ed4d38d
SmartCoding51 <keinakano415@gmail.com>
2022-08-02 22:01:48 +0900

    add DepositSecurityModule deployment code

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -18,8 +18,8 @@ async function main() {
 
   console.log("Oracle deployed at:", oracle.address); */
 
-  /* const depositContractAddr = "0x07b39f4fde4a38bace212b546dac87c58dfe3fdc";
-  const oracleAddr = "0x9a5FdF8146467d70634fc48bEF67dD14B5A08757";
+  const depositContractAddr = "0x07b39f4fde4a38bace212b546dac87c58dfe3fdc";
+  /* const oracleAddr = "0x9a5FdF8146467d70634fc48bEF67dD14B5A08757";
 
   console.log(treasury.address, insuranceFund.address);
   const LightNode = await hre.ethers.getContractFactory("LightNode");
@@ -42,14 +42,34 @@ async function main() {
 
   console.log("NodeOperatorsRegistry deployed at:", nodeOperatorsRegistry.address); */
 
-  const ExecutionLayerRewardsVault = await hre.ethers.getContractFactory("ExecutionLayerRewardsVault");
+  /* const ExecutionLayerRewardsVault = await hre.ethers.getContractFactory("ExecutionLayerRewardsVault");
   const executionLayerRewardsVault = await ExecutionLayerRewardsVault.deploy(
     lightNodeAddr,
     treasury.address
   );
   await executionLayerRewardsVault.deployed();
 
-  console.log("ExecutionLayerRewardsVault deployed at:", executionLayerRewardsVault.address)
+  console.log("ExecutionLayerRewardsVault deployed at:", executionLayerRewardsVault.address); */
+  
+  const nodeOperatorsRegistryAddr = "0x9962eE09d104B338f97F07Ab32F579a94e174025";
+  const networkId = 5; // goerli
+  const maxDepositsPerBlock = 150;
+  const minDepositBlockDistance = 25;
+  const pauseIntentValidityPeriodBlocks = 6646;
+
+  const DepositSecurityModule = await hre.ethers.getContractFactory("DepositSecurityModule");
+  const depositSecurityModule = await DepositSecurityModule.deploy(
+    lightNodeAddr,
+    depositContractAddr,
+    nodeOperatorsRegistryAddr,
+    networkId,
+    maxDepositsPerBlock,
+    minDepositBlockDistance,
+    pauseIntentValidityPeriodBlocks
+  );
+  await depositSecurityModule.deployed();
+
+  console.log("DepositSecurityModule deployed at:", depositSecurityModule.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
