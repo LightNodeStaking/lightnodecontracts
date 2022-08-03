ed6a50a5602bc9ada5ab2c0ec0248b702da0fdb2
SmartCoding51 <keinakano415@gmail.com>
2022-08-04 01:20:27 +0900

    update lightNode test

--- test/lightnode.test.js
+++ test/lightnode.test.js
@@ -3,7 +3,6 @@ const { ethers } = require("hardhat");
 
 describe("LightNode Test Suite", () => {
     let lightNode, oracle, nodeOperatorsRegistry;
-    let deployer;
 
     const depositContractAddr = "0x07b39F4fDE4A38bACe212b546dAc87C58DfE3fDC";
 
@@ -49,5 +48,17 @@ describe("LightNode Test Suite", () => {
 
     it("getOracle()", async() => {
         expect(await lightNode.getOracle()).to.equal(oracle.address);
+    });
+    
+    it("getTreasury()", async() => {
+        expect(await lightNode.getTreasury()).to.equal(treasury.address);
+    });
+    
+    it("getInsuranceFund()", async() => {
+        expect(await lightNode.getInsuranceFund()).to.equal(insuranceFund.address);
+    });
+
+    it("getDepositContract()", async() => {
+        expect(await lightNode.getDepositContract()).to.equal(depositContractAddr);
     });
 });
