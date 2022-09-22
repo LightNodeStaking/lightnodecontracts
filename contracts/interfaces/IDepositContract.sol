1aa44e36f661b91e30bac24d02dcbe2eaf1f22de
keinakano415 <keinakano415@gmail.com>
2022-09-22 21:32:48 +0900

    add deposit contract for unit testing

--- contracts/interfaces/IDepositContract.sol
+++ contracts/interfaces/IDepositContract.sol
@@ -3,6 +3,14 @@ pragma solidity ^0.8.0;
 
 /// @title Deposit contract interface
 interface IDepositContract{
+    /// @notice A processed deposit event.
+    event DepositEvent(
+        bytes pubkey,
+        bytes withdrawal_credentials,
+        bytes amount,
+        bytes signature,
+        bytes index
+    );
     /**
      * @notice Top-ups deposit of a validator on the ETH 2.0 side
      * @param pubkey Validator signing key
@@ -16,4 +24,12 @@ interface IDepositContract{
         bytes /* 96 */ memory signature,
         bytes32 deposit_data_root
     ) external payable;
+
+    /// @notice Query the current deposit root hash.
+    /// @return The deposit root hash.
+    function get_deposit_root() external view returns (bytes32);
+
+    /// @notice Query the current deposit count.
+    /// @return The deposit count encoded as a little endian 64-bit number.
+    function get_deposit_count() external view returns (bytes memory);
 }
